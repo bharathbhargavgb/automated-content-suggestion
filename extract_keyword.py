5618823b2f4ff057e8462799c851415a10ab3b4a
Bharath Bhargav <bharathbhargavgb@gmail.com>
2017-09-23 17:27:16 +0530

    Added script to fetch url from keyword in fetchURL.py and added summarizer to summarize text from a given URL

--- extract_keyword.py
+++ extract_keyword.py
@@ -2,4 +2,7 @@ from topia.termextract import extract
 extractor = extract.TermExtractor()
 inputFile = open("input.txt", 'r')
 text = inputFile.read();
-print sorted(extractor(text))
+keywords = sorted(extractor(text))
+
+for tuples in keywords:
+    print tuples[0]
