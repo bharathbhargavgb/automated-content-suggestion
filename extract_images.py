bb870faf28d149bbb42b5c3b0241fabd17d054a2
Gopal Baaskaran <baaskab@amazon.com>
2017-09-25 02:21:55 +0530

    Integrated image dump feature and synonym words api call

--- extract_images.py
+++ extract_images.py
@@ -3,7 +3,6 @@ import sys    #Importing the System Library
 import os
 
 #This list is used to search keywords. You can edit this list to search for google images of your choice. You can simply add and remove elements of the list.
-search_keyword = ['Neural Network']
 
 #This list is used to further add suffix to your search term. Each element of the list will help you download 100 images. First element is blank which denotes that no suffix is added to the search keyword of the above list. You can edit the list by adding/deleting elements from it.So if the first element of the search_keyword is 'Australia' and the second element of keywords is 'high resolution', then it will search for 'Australia High Resolution'
 keywords = ['']
@@ -64,91 +63,97 @@ def _images_get_all_items(page):
             page = page[end_content:]
     return items
 
-t0 = time.time()   #start the timer
 
-#Download Image Links
-i= 0
-while i<len(search_keyword):
-    items = []
-    iteration = "Item no.: " + str(i+1) + " -->" + " Item name = " + str(search_keyword[i])
-    print (iteration)
-    print ("Evaluating...")
-    search_keywords = search_keyword[i]
-    search = search_keywords.replace(' ','%20')
+def extractImages(search_keyword):
+    t0 = time.time()   #start the timer
+    os.makedirs("/Users/baaskab/hackathon/automated-content-suggestion/images")
+    #Download Image Links
+    print search_keyword
+    i= 0
+    while i<len(search_keyword):
+        items = []
+        iteration = "Item no.: " + str(i+1) + " -->" + " Item name = " + str(search_keyword[i])
+        print (iteration)
+        print ("Evaluating...")
+        search_keywords = search_keyword[i]
+        search = search_keywords.replace(' ','%20')
 
-     #make a search keyword  directory
-    try:
-        os.makedirs(search_keywords)
-    except OSError, e:
-        if e.errno != 17:
-            raise
-        # time.sleep might help here
-        pass
-
-    j = 0
-    while j<len(keywords):
-        pure_keyword = keywords[j].replace(' ','%20')
-        url = 'https://www.google.com/search?q=' + search + pure_keyword + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
-        raw_html =  (download_page(url))
-        time.sleep(0.1)
-        items = items + (_images_get_all_items(raw_html))
-        j = j + 1
-    print ("Image Links = "+str(items))
-    print ("Total Image Links = "+str(len(items)))
-    print ("\n")
-
-
-    #This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
-    info = open('output.txt', 'a')        #Open the text file called database.txt
-    info.write(str(i) + ': ' + str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")         #Write the title of the page
-    info.close()                            #Close the file
-
-    t1 = time.time()    #stop the timer
-    total_time = t1-t0   #Calculating the total time required to crawl, find and download all the links of 60,000 images
-    print("Total time taken: "+str(total_time)+" Seconds")
-    print ("Starting Download...")
-
-    ## To save imges to the same directory
-    # IN this saving process we are just skipping the URL if there is any error
-
-    k=0
-    errorCount=0
-    while(k < len(items) and k < 5):
-        from urllib2 import Request,urlopen
-        from urllib2 import URLError, HTTPError
-
+         #make a search keyword  directory
         try:
-            req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
-            response = urlopen(req,None,15)
-            output_file = open(search_keywords+"/"+str(k+1)+".jpg",'wb')
+            os.makedirs("/Users/baaskab/hackathon/automated-content-suggestion/images/" + search_keywords)
+        except OSError, e:
+            if e.errno != 17:
+                raise
+            # time.sleep might help here
+            pass
 
-            data = response.read()
-            output_file.write(data)
-            response.close();
+        j = 0
+        while j<len(keywords):
+            pure_keyword = keywords[j].replace(' ','%20')
+            url = 'https://www.google.com/search?q=' + search + pure_keyword + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
+            raw_html =  (download_page(url))
+            time.sleep(0.1)
+            items = items + (_images_get_all_items(raw_html))
+            j = j + 1
+        print ("Image Links = "+str(items))
+        print ("Total Image Links = "+str(len(items)))
+        print ("\n")
 
-            print("completed ====> "+str(k+1))
 
-            k=k+1;
+        #This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
+        #info = open('output.txt', 'a')        #Open the text file called database.txt
+        #info.write(str(i) + ': ' + str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")         #Write the title of the page
+        #info.close()                            #Close the file
 
-        except IOError:   #If there is any IOError
+        t1 = time.time()    #stop the timer
+        total_time = t1-t0   #Calculating the total time required to crawl, find and download all the links of 60,000 images
+        print("Total time taken: "+str(total_time)+" Seconds")
+        print ("Starting Download...")
 
-            errorCount+=1
-            print("IOError on image "+str(k+1))
-            k=k+1;
+        ## To save imges to the same directory
+        # IN this saving process we are just skipping the URL if there is any error
 
-        except HTTPError as e:  #If there is any HTTPError
+        k=0
+        errorCount=0
+        while(k < len(items) and k < 5):
+            from urllib2 import Request,urlopen
+            from urllib2 import URLError, HTTPError
 
-            errorCount+=1
-            print("HTTPError"+str(k))
-            k=k+1;
-        except URLError as e:
+            try:
+                req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
+                response = urlopen(req,None,15)
+                output_file = open("/Users/baaskab/hackathon/automated-content-suggestion/images/" + search_keywords+"/"+str(k+1)+".jpg",'wb')
 
-            errorCount+=1
-            print("URLError "+str(k))
-            k=k+1;
+                data = response.read()
+                output_file.write(data)
+                response.close();
 
-    i = i+1
+                print("completed ====> "+str(k+1))
 
-print("\n")
-print(" Download Complete!")
-print("\n"+str(errorCount)+" ----> total Errors")
+                k=k+1;
+
+            except IOError:   #If there is any IOError
+
+                errorCount+=1
+                print("IOError on image "+str(k+1))
+                k=k+1;
+
+            except HTTPError as e:  #If there is any HTTPError
+
+                errorCount+=1
+                print("HTTPError"+str(k))
+                k=k+1;
+            except URLError as e:
+
+                errorCount+=1
+                print("URLError "+str(k))
+                k=k+1;
+
+        i = i+1
+
+    print("\n")
+    print(" Download Complete!")
+    print("\n"+str(errorCount)+" ----> total Errors")
+
+
+#extractImages(['Rahman'])
